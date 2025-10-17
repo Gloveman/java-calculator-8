37458f7aedcbc62cc38895e88b6e42ec3ee33322
gloveman <gloveman@postech.ac.kr>
2025-10-18 03:38:40 +0900

    feat(main): 기본 구분자 처리, 결과 출력 구현
    체크리스트 중 다음 두 가지 기능 구현:
    - 기본 구분자인 쉼표, 콜론으로 숫자를 분리한 뒤 합 구하기
    - 결과: {answer} 형식으로 출력하기

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
-
+import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String test=Console.readLine(); //입력받기
+        String[] numbers=test.split("[,:]"); //기본 구분자로 숫자 분리
+        int result=0;
+        for(String num:numbers){
+            result+=Integer.parseInt(num); //숫자만 더함
+        }
+
+        System.out.println("결과 :"+" "+result); //결과 출력
+
     }
 }
